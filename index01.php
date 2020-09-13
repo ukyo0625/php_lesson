5aa53fe713116a0217d9f326f328f38693515727
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-13 21:10:47 +0900

    add_variable

--- index01.php
+++ index01.php
@@ -1,3 +1,18 @@
+<?php
+ $msg = "<p>毎日プログラミングの勉強を頑張る！</p>";
+ $msg .= "<p>まずはPHPだ！</p>";
+ $age = 28;
+ $age = "<p>私は{$age}の男です。</p>";
+ $mission = <<<eof
+  <h2>学習内容</h2>
+  <ul>
+    <li>PHP</li>
+    <li>java</li>
+    <li>java script</li>
+  </ul>
+eof;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +20,11 @@
   <meta charset="utf-8">
 </head>
 <body>
-  <h1>PHPの基本</h1>
-  <?php echo "PHP初記述！"; ?>
+<h1>PHP</h1>
+<?php
+  echo $msg;
+  echo $age;
+  echo $mission;
+?>
 </body>
 </html>
