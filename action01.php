1008c0b4fc10953391e0728f1a4b027dc35cc02f
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-14 21:48:21 +0900

    add_action_lesson

---
+++ action01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <h1>動的なWEBページの作成</h1>
+  <form method="post" action="">
+    <label>名前<input type="text" name="username" required></label>
+    <input type="submit" name="exe" value="実行">
+  </form>
+
+</body>
+</html>
---
+++ action02.php
@@ -0,0 +1,23 @@
+<?php
+ if(isset($_POST['exe'])){
+ echo "実行ボタンが押されました";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <h1>動的なWEBページの作成</h1>
+  <form method="post" action="">
+    <label>名前<input type="text" name="username" required></label>
+    <input type="submit" name="exe" value="実行">
+  </form>
+
+</body>
+</html>
