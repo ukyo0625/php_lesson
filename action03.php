d4801566040a98c9534042c04af4eb0bb46437d9
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-18 18:31:40 +0900

    add_action03.php

---
+++ action03.php
@@ -0,0 +1,25 @@
+<?php
+ if(isset($_POST['exe'])){
+  $name = $_POST['username'];
+  $name = "<p>{$name}さんですね。</p>";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <h1>動的なWEBページの作成</h1>
+  <form method="post" action="">
+    <label>名前<input type="text" name="username" required></label>
+    <input type="submit" name="exe" value="実行">
+  </form>
+  <?php echo $name; ?>
+
+</body>
+</html>
